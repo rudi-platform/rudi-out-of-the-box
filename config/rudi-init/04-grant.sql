93b9faa36c0761172be7cc84fd7242cbc933d7d7
fni18300 <fni18300@open-groupe.com>
2024-10-28 16:23:56 +0100

    modification des mots de passe par défaut des utilisateurs

---
+++ config/rudi-init/04-grant.sql
@@ -0,0 +1,26 @@
+GRANT ALL ON SCHEMA public TO acl;
+GRANT ALL ON SCHEMA acl_data TO acl;
+
+GRANT ALL ON SCHEMA public TO kalim;
+GRANT ALL ON SCHEMA kalim_data TO kalim;
+
+GRANT ALL ON SCHEMA public TO konsent;
+GRANT ALL ON SCHEMA konsent_data TO konsent;
+
+GRANT ALL ON SCHEMA public TO kos;
+GRANT ALL ON SCHEMA kos_data TO kos;
+
+GRANT ALL ON SCHEMA public TO selfdata;
+GRANT ALL ON SCHEMA selfdata_data TO selfdata;
+
+GRANT ALL ON SCHEMA public TO strukture;
+GRANT ALL ON SCHEMA strukture_data TO strukture;
+
+GRANT ALL ON SCHEMA public TO template;
+GRANT ALL ON SCHEMA template_data TO template;
+
+GRANT ALL ON SCHEMA public TO apigateway;
+GRANT ALL ON SCHEMA apigateway_data TO apigateway;
+
+GRANT ALL ON SCHEMA public TO projekt;
+GRANT ALL ON SCHEMA projekt_data TO projekt;
